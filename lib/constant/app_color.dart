d2a4d2e801306c31b111d11c2d724e89524628a7
imamprasetya <imamprasetya06@gmail.com>
2026-03-03 23:18:36 +0700

    login

---
+++ lib/constant/app_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class AppColor {
+  static const Color black = Color(0xFF000020);
+  static const Color navy = Color(0xFF000033);
+  static const Color lightBlue = Color(0xFFD6EDFF);
+  static const Color white = Color(0xFFE0E0E0);
+
+  //darkmode
+  static const Color navyLight = Color.fromARGB(255, 12, 12, 35);
+
+}
